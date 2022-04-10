530221262aa4422f65e7de2ac1a42873d68abe2b
srivarshinibubbles <79245102+srivarshinibubbles@users.noreply.github.com>
2022-04-10 12:02:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+noseX = 0;
+noseY = 0;
+difference= 0;
+leftWristX = 0;
+rightWristX = 0;
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560,156);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+
+
+}
+
+function draw()
+{
+
+    background('#808080');
+    fill('#FFC0CB');
+    stroke("black");
+    textSize(difference);
+    text("Srivarshini" , 20 , 100);
+    
+    
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is Initialized")
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY)
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.leftWrist.y;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + "rightWristX = "+ rightWristX + "difference = " + difference);
+    }
+}
+
+
+
